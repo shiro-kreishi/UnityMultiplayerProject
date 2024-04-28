3b520c553a747e05d9e25cc9e1edab2da376f735
shiro-kreishi <vadas25@yandex.ru>
2024-04-28 20:59:33 +0500

    add player hud

---
+++ Assets/Scripts/PlayerHud.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerHud : NetworkBehaviour
+{
+    [SerializeField] private NetworkVariable<NetworkString> playersName = new NetworkVariable<NetworkString>();
+    private bool overlaySet = false;
+    [SerializeField] private TextMeshProUGUI localPlayerOverlay;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            playersName.Value = $"Player {OwnerClientId}";
+        }
+    }
+
+    public void SetOverlay()
+    {
+        localPlayerOverlay.text = playersName.Value;
+        // Logger.Instance.LogInfo(playersName.Value);
+    }
+
+    private void Update()
+    {
+        if (!overlaySet && !string.IsNullOrEmpty(playersName.Value))
+        {
+            SetOverlay();
+            overlaySet = true;
+        }
+    }
+}
+
